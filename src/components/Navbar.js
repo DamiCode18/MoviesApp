7cba0e67733304e41e5ea1712c53c12c36b61039
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-10-09 23:38:24 +0100

    updated icon and started search input

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Text, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Text, Flex, Spacer, Input } from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
 
 export default function Navbar() {
   return (
@@ -7,14 +8,18 @@ export default function Navbar() {
         <Box mr={10}><Text color="#e50914" fontWeight={700} fontSize={25} cursor="pointer">NETFLIX</Text></Box>
         <Box mt={2}>
             <Flex>
-            <Text mr={4} color="#fff" fontWeight={500} fontSize={14}>Home</Text>
             <Text mr={4}  color="#fff" fontWeight={500} fontSize={14}>TV Shows</Text>
             <Text mr={4} color="#fff" fontWeight={500} fontSize={14}>New & Popular</Text>
             <Text mr={4} color="#fff" fontWeight={500} fontSize={14}>My List</Text>
-            <Text color="#fff" fontWeight={500} fontSize={14}>Browse</Text>
             </Flex>
         </Box>
         <Spacer />
+        <Box>
+            <Flex>
+            <Input type="text" />
+            <Search2Icon />
+            </Flex>
+        </Box>
     </Flex>
   )
 }
